a0a181ac92996a912386786e5c25ad2c60082c49
Sophia Xie <sxie@login-d.ccm.sickkids.ca>
2024-05-15 10:39:03 -0400

    test pipeline on mpro binder

---
+++ scripts/parse_fasta.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+### helper file to convert ProteinMPNN output into alphafold input
+### REQUIRED ARGUMENT: path to .fasta file to be parsed
+
+# parsed files will be written in same directory
+fasta_dir=$(dirname $1)
+
+echo FASTA file directory: $fasta_dir
+
+skip_first=true # ignore original sequence
+
+# read file line by line
+while IFS= read -r line; do
+	#  heck if line starts with '>' (indicates new sequence)
+    if [[ $line == ">"* ]]; then
+        # skip the first sequence
+        if [ "$skip_first" = true ]; then
+            continue
+        fi
+        # get sample number from the sequence identifier
+        sample_num=$(echo "$line" | awk -F '[=,]' '{print $4}')
+        # Create a new fasta file with the sample number as the name
+        echo "$line" > $fasta_dir/sample_${sample_num}.fasta
+    else
+	# skip the first sequence
+	if [ "$skip_first" = true ]; then
+            skip_first=false
+            continue
+        fi
+	    # extract the sequence before the slash (in the case of binders, excludes the target protein)
+        sequence=$(echo "$line" | cut -d'/' -f1)
+        # append the sequence to the corresponding fasta file
+        echo "$sequence" >> $fasta_dir/sample_${sample_num}.fasta
+    fi
+done < $1
+
+echo
+echo Done parsing FASTA file.
---
+++ scripts/test_RFdiffusion_pipeline.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#SBATCH -G 1 --mem=8G
+#SBATCH --job-name test_RFdiffusion_pipeline
+
+# This shell script runs the pipeline from RFDiffusion -> ProteinMPNN -> alphafold.
+
+input_dir=$HOME/RFdiffusion_pipeline/inputs/
+output_dir=$HOME/RFdiffusion_pipeline/test_outputs/mpro_binder/
+
+# Important: Files with the same name may not be overwritten.
+
+RFDiffusion_dir=/hpf/tools/alma8/RFDiffusion/1.1.0/
+ProteinMPNN_dir=/hpf/tools/alma8/ProteinMPNN/v1.0.1/
+alphafold_dir=/hpf/tools/centos7/alphafold/2.3.2/
+
+# Step 1: RFDiffusion to generate a structure
+
+echo Loading RFDiffusion module...
+
+module load RFDiffusion/1.1.0
+
+echo
+echo Activating conda...
+
+eval "$(conda shell.bash hook)"
+conda activate /hpf/tools/centos7/miniforge/3/envs/SE3nv
+
+echo
+echo Running RFDiffusion script...
+
+$RFDiffusion_dir/scripts/run_inference.py \
+inference.output_prefix=$output_dir/RFDiffusion/RFDiffusion \
+inference.num_designs=1 \
+'contigmap.contigs=[A1-305/0 25-50]' \
+inference.input_pdb=$input_dir/pdbs/6w63.pdb \
+'ppi.hotspot_res=[A41,A145]' \
+denoiser.noise_scale_ca=0 \
+denoiser.noise_scale_frame=0
+
+echo
+echo Done RFDiffusion.
+
+# Step 2: ProteinMPNN to retrieve the amino acid sequence
+
+echo
+echo Loading ProteinMPNN module...
+
+module load ProteinMPNN/v1.0.1
+
+echo Running ProteinMPNN script...
+
+python $ProteinMPNN_dir/protein_mpnn_run.py \
+--pdb_path $output_dir/RFDiffusion/RFDiffusion_0.pdb \
+--out_folder $output_dir/ProteinMPNN/ \
+--num_seq_per_target 1
+--sampling_temp "0.0001"
+
+echo
+echo Done ProteinMPNN.
+
+# Step 3: alphafold to regenerate the structure using the sequence
+
+# Prepare output from ProteinMPNN for alphafold
+echo
+echo Parsing FASTA file...
+bash ./parse_fasta.sh $output_dir/ProteinMPNN/seqs/RFDiffusion_0.fa
+
+echo
+echo Loading alphafold module...
+
+module load alphafold/2.3.2
+
+echo
+echo Running alphafold script...
+
+alphafold \
+--fasta_paths=$output_dir/ProteinMPNN/seqs/sample_1.fasta \
+--output_dir=$output_dir/alphafold/ \
+--data_dir=$alphafold_dir/data/ \
+--max_template_date=2024-05-01 \
+--uniref90_database_path=$alphafold_dir/data/uniref90/uniref90.fasta \
+--mgnify_database_path=$alphafold_dir/data/mgnify/mgy_clusters_2022_05.fa \
+--template_mmcif_dir=$alphafold_dir/data/pdb_mmcif/mmcif_files \
+--obsolete_pdbs_path=$alphafold_dir/data/pdb_mmcif/obsolete.dat \
+--use_gpu_relax=true \
+--db_preset=reduced_dbs \
+--small_bfd_database_path=$alphafold_dir/data/small_bfd/bfd-first_non_consensus_sequences.fasta \
+--pdb70_database_path=$alphafold_dir/data/pdb70/pdb70
+
+echo
+echo Done alphafold.
+
+echo
+echo Job done.
