774ae235d5fc3be9cb807b3578adb6f9617f221f
Connor Sicheri <144295774+connorsicheri@users.noreply.github.com>
2024-07-03 17:09:07 -0400

    Update select_residues_PPinterface.py
    modify this program to have more desirable properties. updated output structure

--- helper_scripts/residue_selection/select_residues_PPinterface.py
+++ helper_scripts/residue_selection/select_residues_PPinterface.py
@@ -6,16 +6,16 @@ import os
 # List of hydrophobic residues
 HYDROPHOBIC_RESIDUES = ['GLY', 'ALA', 'VAL', 'LEU', 'ILE', 'PRO', 'PHE', 'MET', 'TRP']
 
-def get_hydrophobic_beta_carbons(structure):
+
+def get_beta_carbons(structure):
     beta_carbons = []
     for model in structure:
         for chain in model:
             for residue in chain:
-                if residue.get_resname() in HYDROPHOBIC_RESIDUES:
-                    if 'CB' in residue:
-                        beta_carbons.append(residue['CB'])
-                    elif 'CA' in residue and residue.get_resname() == 'GLY':  # Glycine has no CB
-                        beta_carbons.append(residue['CA'])
+                if 'CB' in residue:
+                    beta_carbons.append(residue['CB'])
+                elif 'CA' in residue and residue.get_resname() == 'GLY':  # Glycine has no CB
+                    beta_carbons.append(residue['CA'])
     return beta_carbons
 
 def calculate_distances(cb_atoms_1, cb_atoms_2):
@@ -26,18 +26,19 @@ def calculate_distances(cb_atoms_1, cb_atoms_2):
             distances.append((distance, atom1.get_parent(), atom2.get_parent()))
     return distances
 
-def format_residue(residue):
+def format_residue(residue, distance):
     chain_id = residue.get_parent().id
     resnum = residue.id[1]
-    return "{}\t{}".format(chain_id, resnum)
+    hydro = "hydrophobic" if (residue.get_id() in HYDROPHOBIC_RESIDUES) else "NON-hydrophobic"
+    return "{}\t{}\t{}\t{}".format(chain_id, resnum, distance, hydro)
 
 def get_top_closest_pairs(pdb_file1, pdb_file2, top_n, output_dir):
     parser = PDBParser(QUIET=True)
     structure1 = parser.get_structure('protein1', pdb_file1)
     structure2 = parser.get_structure('protein2', pdb_file2)
     
-    cb_atoms_1 = get_hydrophobic_beta_carbons(structure1)
-    cb_atoms_2 = get_hydrophobic_beta_carbons(structure2)
+    cb_atoms_1 = get_beta_carbons(structure1)
+    cb_atoms_2 = get_beta_carbons(structure2)
     
     distances = calculate_distances(cb_atoms_1, cb_atoms_2)
     distances.sort(key=lambda x: x[0])
@@ -50,9 +51,9 @@ def get_top_closest_pairs(pdb_file1, pdb_file2, top_n, output_dir):
     protein_name = filename.split('_')[0]
 
     with open((output_dir + "/" + protein_name + "_hotspots.txt"), 'w') as out_file:
-        out_file.write("chain_identifier\tresidue_sequence_number\n")  # Writing column headers
+        out_file.write("chain_identifier\tresidue_sequence_number\tdistance\thydrophobic\n")  # Writing column headers
         for distance, res1, res2 in distances[:top_n]:
-            res1_info = format_residue(res1)
+            res1_info = format_residue(res1, distance)
             out_file.write(res1_info + '\n')
 
 if __name__ == "__main__":
