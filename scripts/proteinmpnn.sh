b4f24f5c49cabfd44478e046d9694d046edf0305
Sophia Xie <xiesophia7@gmail.com>
2024-07-13 23:32:03 -0400

    update logging

--- scripts/proteinmpnn.sh
+++ scripts/proteinmpnn.sh
@@ -7,8 +7,6 @@ repo_dir=/hpf/tools/alma8/dl_binder_design/v1.0.1/
 echo Loading module...
 module load dl_binder_design/v1.0.1
 
-echo Running script...
-
 run_name=$1
 output_dir=$2
 seqs_per_struct=$3
@@ -18,6 +16,8 @@ echo RUN_NAME $run_name
 echo OUTPUT_DIR $output_dir
 echo SEQS_PER_STRUCT $seqs_per_struct
 echo INPUT_DIR $input_dir
+
+echo Running script...
 
 $repo_dir/base_dir/mpnn_fr/dl_interface_design.py \
 -pdbdir $input_dir \
