4261f5cbefc25251f5c565cae7ac23ae1a3f1bc0
Sophia Xie <xiesophia7@gmail.com>
2024-07-25 10:09:21 -0400

    specify required arguments

--- run_pipeline.py
+++ run_pipeline.py
@@ -36,18 +36,21 @@ def main(args):
 
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
 
-    argparser.add_argument("--run_name", type=str, default="test_run", help="Name of run. Must be unique as this is used to name the output files and directory (Default: test_run)")
-    argparser.add_argument("--path_to_pdb", type=str, required=True, help="Path to target PDB. Note that RFdiffusion will only process standard residues designated ATOM (Required)")
-    argparser.add_argument("--hotspots", type=str, required=True, help='Hotspot residues. Comma-separated list of <chain_id><residue_index>, no spaces e.g. "A30,A33,A34" (Required)') 
-    argparser.add_argument("--min_length", type=int, default=50, help="Minimum binder length (Default: 50)")
-    argparser.add_argument("--max_length", type=int, default=90, help="Maximum binder length (Default: 90)")
-    argparser.add_argument("--num_structs", type=int, default=2, help="Number of RFdiffusion structures to generate (Default: 2)")
-    argparser.add_argument("--sequences_per_struct", type=int, default=2, help="Number of sequences to generate per structure (Default: 2)")
-    argparser.add_argument("--output_dir", type=str, default=".", help="Output directory (Default: current directory)")
-    argparser.add_argument("--scaffold_dir", type=str, help="Scaffold directory if using fold conditioning. If this is provided we will ignore min_length and max_length. (Default: None)")
-    argparser.add_argument("--sbatch_flags", default="-G 1 --mem=8G --time=2:00:00", help='Flags to pass to sbatch command. GPU is required (Default: "-gpus 1 --mem=8G --time=2:00:00")')
+    optional.add_argument("--run_name", type=str, default="test_run", help="Name of run. Must be unique as this is used to name the output files and directory (default: test_run)")
+    required.add_argument("--path_to_pdb", type=str, required=True, help="Path to target PDB. Note that RFdiffusion will only process standard residues designated ATOM.")
+    required.add_argument("--hotspots", type=str, required=True, help='Hotspot residues. Comma-separated list of <chain_id><residue_index>, no spaces e.g. "A30,A33,A34"') 
+    optional.add_argument("--min_length", type=int, default=50, help="Minimum binder length (default: 50)")
+    optional.add_argument("--max_length", type=int, default=90, help="Maximum binder length (default: 90)")
+    optional.add_argument("--num_structs", type=int, default=2, help="Number of RFdiffusion structures to generate (default: 2)")
+    optional.add_argument("--sequences_per_struct", type=int, default=2, help="Number of sequences to generate per structure (default: 2)")
+    optional.add_argument("--output_dir", type=str, default=".", help="Output directory (default: current directory)")
+    optional.add_argument("--scaffold_dir", type=str, help="Scaffold directory if using fold conditioning. If this is provided we will ignore min_length and max_length. (default: None)")
+    optional.add_argument("--sbatch_flags", default="-G 1 --mem=8G --time=2:00:00", help='Flags to pass to sbatch command. GPU is required (default: "-gpus 1 --mem=8G --time=2:00:00")')
 
-    args = argparser.parse_args()    
+    parser._action_groups.append(optional)
+    args = parser.parse_args()    
     main(args)   
